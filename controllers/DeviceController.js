7ba7886ee74523cd90e26383654ed7190ee3110a
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-03-21 21:47:53 -0300

    finishing the device controller

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -2,10 +2,22 @@ const device = require ('../models/Device');
 
 class DeviceController {
    
-    async createDevice(deviceName) {
+    static async createDeviceAsync(deviceName) {
         const returnedObject = await device.create({name: deviceName});
         console.log('New device created: ' + deviceName);
         return returnedObject;
+    }
+
+    static async getAllDevicesAsync () {
+        return await device.find({});
+    }
+
+    static async deleteDeviceAsync (id) {
+        return await device.deleteOne({_id: id});
+    }
+
+    static async updateDeviceAsync (newDevice) {
+        return await device.updateOne({_id: newDevice.id}, {name: newDevice.name});
     }
 
 }
--- routes/devicesRoute.js
+++ routes/devicesRoute.js
@@ -5,16 +5,51 @@ const InvalidField = require('../errorTreatment/InvalidField');
 const router = Router();
 
 router
-    .post('/devices', (req, res, next) => { saveNewDeviceAsync(req, res, next) });
+    .post('/devices', (req, res, next) => { saveNewDeviceAsync(req, res, next) })
+    .get('/devices', (req, res, next) => { getAllDevicesAsync(req, res, next) })
+    .delete('/devices/:deviceId', (req, res, next) => { deleteDeviceAsync(req, res, next) })
+    .put('/devices', (req, res, next) => { updateDeviceAsync(req, res, next) });
 
 
 async function saveNewDeviceAsync(req, res, next) {
     try {
         const reqBody = req.body;
         validateRequest(reqBody);
-        var device = new DeviceController();
-        const deviceCreated = await device.createDevice(reqBody.name);
+        const deviceCreated = await DeviceController.createDeviceAsync(reqBody.name);
         return res.status(200).json(deviceCreated);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+async function getAllDevicesAsync(req, res, next) {
+    try {
+        const devices = await DeviceController.getAllDevicesAsync();
+        return res.status(200).json(devices);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+async function deleteDeviceAsync(req, res, next) {
+    try {
+        const deviceId = req.params.deviceId;
+        const deletedObject = await DeviceController.deleteDeviceAsync(deviceId);
+        return res.status(200).json(`The number of deleted devices was: ${deletedObject.deletedCount}`);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+async function updateDeviceAsync(req, res, next) {
+    try {
+        const reqBody = req.body;
+        validateRequest(reqBody);
+        const deviceUpdated = await DeviceController.updateDeviceAsync(reqBody);
+        return res.status(200).json(`The number of updated devices was: ${deviceUpdated.modifiedCount}`);
     }
     catch (error) {
         next(error);
