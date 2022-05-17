fedc195d65e2f936ae607959ca13363ad7617edb
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-05-16 23:48:38 -0300

    changing device table

--- models/Device.js
+++ models/Device.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const Device = mongoose.model('Devices', {
     name: String,
-    latitude: String,
-    longiude: String,
+    latitude: Number,
+    longiude: Number,
+    isActive: { type: Boolean, default: true },
     created_at: { type: Date, default: Date.now }
 });
 
