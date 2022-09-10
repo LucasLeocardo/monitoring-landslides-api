57143e5dad4003ab2c538a003b51e269df8bce23
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-09-10 20:01:05 -0300

    Enhancing real time data  update

--- shared/socketQueries.js
+++ shared/socketQueries.js
@@ -23,128 +23,140 @@ module.exports = async (io, socket) => {
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.LINEAR_ACCELERATION)) {
         const linearAccelerationMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.LINEAR_ACCELERATION).measurementTypeId;
-        const senderLinearAccelerationDataInterval = setInterval(async () => {
-            const lastLinearAccelerationData = await VibrationController.getLastLinearAccelerationByDeviceIdAsync(deviceId, linearAccelerationMeasurementTypeId);
-            if (lastLinearAccelerationData) {
-                if (lastLinearAccelerationData._id.toString() !== lastLinearAccelerationDataId) {
-                    socket.volatile.emit("linear-acceleration-data", lastLinearAccelerationData.value);
+        setTimeout(() => {
+            const senderLinearAccelerationDataInterval = setInterval(async () => {
+                const lastLinearAccelerationData = await VibrationController.getLastLinearAccelerationByDeviceIdAsync(deviceId, linearAccelerationMeasurementTypeId);
+                if (lastLinearAccelerationData) {
+                    if (lastLinearAccelerationData._id.toString() !== lastLinearAccelerationDataId) {
+                        socket.volatile.emit("linear-acceleration-data", lastLinearAccelerationData.value);
+                    }
+                    lastLinearAccelerationDataId = lastLinearAccelerationData._id.toString();
                 }
-                lastLinearAccelerationDataId = lastLinearAccelerationData._id.toString();
-            }
-            else {
-                if (lastLinearAccelerationDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("linear-acceleration-data", value);
-                    lastLinearAccelerationDataId = "-1";
+                else {
+                    if (lastLinearAccelerationDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("linear-acceleration-data", value);
+                        lastLinearAccelerationDataId = "-1";
+                    }
                 }
-            }
-        }, 3000);
-        dataIntervalList.push(senderLinearAccelerationDataInterval);
+            }, 1000);
+            dataIntervalList.push(senderLinearAccelerationDataInterval);
+          }, 3000);
     }
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.ANGULAR_VELOCITY)) {
         const angularVelocityMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.ANGULAR_VELOCITY).measurementTypeId;
-        const senderAngularVelocityDataInterval = setInterval(async () => {
-            const lastAngularVelocityData = await VibrationController.getLastAngularVelocityByDeviceIdAsync(deviceId, angularVelocityMeasurementTypeId);
-            if (lastAngularVelocityData) {
-                if (lastAngularVelocityData._id.toString() !== lastAngularVelocityDataId) {
-                    socket.volatile.emit("angular-velocity-data", lastAngularVelocityData.value);
+        setTimeout(() => {
+            const senderAngularVelocityDataInterval = setInterval(async () => {
+                const lastAngularVelocityData = await VibrationController.getLastAngularVelocityByDeviceIdAsync(deviceId, angularVelocityMeasurementTypeId);
+                if (lastAngularVelocityData) {
+                    if (lastAngularVelocityData._id.toString() !== lastAngularVelocityDataId) {
+                        socket.volatile.emit("angular-velocity-data", lastAngularVelocityData.value);
+                    }
+                    lastAngularVelocityDataId = lastAngularVelocityData._id.toString();
                 }
-                lastAngularVelocityDataId = lastAngularVelocityData._id.toString();
-            }
-            else {
-                if (lastAngularVelocityDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("angular-velocity-data", value);
-                    lastAngularVelocityDataId = "-1";
+                else {
+                    if (lastAngularVelocityDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("angular-velocity-data", value);
+                        lastAngularVelocityDataId = "-1";
+                    }
                 }
-            }
+            }, 1000);
+            dataIntervalList.push(senderAngularVelocityDataInterval);
         }, 3000);
-        dataIntervalList.push(senderAngularVelocityDataInterval);
     }
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.TEMPERATURE)) {
         const temperatureMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.TEMPERATURE).measurementTypeId;
-        const senderTemperatureDataInterval = setInterval(async () => {
-            const lastTemperatureData = await TemperatureController.getLastMesurementByDeviceIdAsync(deviceId, temperatureMeasurementTypeId);
-            if (lastTemperatureData) {
-                if (lastTemperatureData._id.toString() !== lastTemperatureDataId) {
-                    socket.volatile.emit("temperature-data", lastTemperatureData.value);
+        setTimeout(() => {
+            const senderTemperatureDataInterval = setInterval(async () => {
+                const lastTemperatureData = await TemperatureController.getLastMesurementByDeviceIdAsync(deviceId, temperatureMeasurementTypeId);
+                if (lastTemperatureData) {
+                    if (lastTemperatureData._id.toString() !== lastTemperatureDataId) {
+                        socket.volatile.emit("temperature-data", lastTemperatureData.value);
+                    }
+                    lastTemperatureDataId = lastTemperatureData._id.toString();
                 }
-                lastTemperatureDataId = lastTemperatureData._id.toString();
-            }
-            else {
-                if (lastTemperatureDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("temperature-data", value);
-                    lastTemperatureDataId = "-1";
+                else {
+                    if (lastTemperatureDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("temperature-data", value);
+                        lastTemperatureDataId = "-1";
+                    }
                 }
-            }
+            }, 1000);
+            dataIntervalList.push(senderTemperatureDataInterval);
         }, 3000);
-        dataIntervalList.push(senderTemperatureDataInterval);
     }
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.HUMIDITY)) {
         const humidityMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.HUMIDITY).measurementTypeId;
-        const senderHumidityDataInterval = setInterval(async () => {
-            const lastHumidityData = await HumidityController.getLastMesurementByDeviceIdAsync(deviceId, humidityMeasurementTypeId);
-            if (lastHumidityData) {
-                if (lastHumidityData._id.toString() !== lastHumidityDataId) {
-                    socket.volatile.emit("humidity-data", lastHumidityData.value);
+        setTimeout(() => {
+            const senderHumidityDataInterval = setInterval(async () => {
+                const lastHumidityData = await HumidityController.getLastMesurementByDeviceIdAsync(deviceId, humidityMeasurementTypeId);
+                if (lastHumidityData) {
+                    if (lastHumidityData._id.toString() !== lastHumidityDataId) {
+                        socket.volatile.emit("humidity-data", lastHumidityData.value);
+                    }
+                    lastHumidityDataId = lastHumidityData._id.toString();
                 }
-                lastHumidityDataId = lastHumidityData._id.toString();
-            }
-            else {
-                if (lastHumidityDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("humidity-data", value);
-                    lastHumidityDataId = "-1";
+                else {
+                    if (lastHumidityDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("humidity-data", value);
+                        lastHumidityDataId = "-1";
+                    }
                 }
-            }
+            }, 1000);
+            dataIntervalList.push(senderHumidityDataInterval);
         }, 3000);
-        dataIntervalList.push(senderHumidityDataInterval);
     }
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.RAINFALL_LEVEL)) {
         const rainfallLevelMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.RAINFALL_LEVEL).measurementTypeId;
-        const senderRainFallLevelDataInterval = setInterval(async () => {
-            const lastRainFallLevelData = await RainfallLevelController.getLastMesurementByDeviceIdAsync(deviceId, rainfallLevelMeasurementTypeId);
-            if (lastRainFallLevelData) {
-                if (lastRainFallLevelData._id.toString() !== lastRainfallLevelDataId) {
-                    socket.volatile.emit("rainfall-level-data", lastRainFallLevelData.value);
+        setTimeout(() => {
+            const senderRainFallLevelDataInterval = setInterval(async () => {
+                const lastRainFallLevelData = await RainfallLevelController.getLastMesurementByDeviceIdAsync(deviceId, rainfallLevelMeasurementTypeId);
+                if (lastRainFallLevelData) {
+                    if (lastRainFallLevelData._id.toString() !== lastRainfallLevelDataId) {
+                        socket.volatile.emit("rainfall-level-data", lastRainFallLevelData.value);
+                    }
+                    lastRainfallLevelDataId = lastRainFallLevelData._id.toString();
                 }
-                lastRainfallLevelDataId = lastRainFallLevelData._id.toString();
-            }
-            else {
-                if (lastRainfallLevelDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("rainfall-level-data", value);
-                    lastRainfallLevelDataId = "-1";
+                else {
+                    if (lastRainfallLevelDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("rainfall-level-data", value);
+                        lastRainfallLevelDataId = "-1";
+                    }
                 }
-            }
+            }, 1000);
+            dataIntervalList.push(senderRainFallLevelDataInterval);
         }, 3000);
-        dataIntervalList.push(senderRainFallLevelDataInterval);
     }
 
     if (deviceMeasurementTypes.includes(MeasurementTypes.PRESSURE)) {
         const pressureMeasurementTypeId = deviceMeasuredDataTypes.find(deviceMeasuredDataType => deviceMeasuredDataType.measurementType === MeasurementTypes.PRESSURE).measurementTypeId;
-        const senderPressureDataInterval = setInterval(async () => {
-            const lastPressureData = await PressureController.getLastMesurementByDeviceIdAsync(deviceId, pressureMeasurementTypeId);
-            if (lastPressureData) {
-                if (lastPressureData._id.toString() !== lastPressureDataId) {
-                    socket.volatile.emit("pressure-data", lastPressureData.value);
+        setTimeout(() => {
+            const senderPressureDataInterval = setInterval(async () => {
+                const lastPressureData = await PressureController.getLastMesurementByDeviceIdAsync(deviceId, pressureMeasurementTypeId);
+                if (lastPressureData) {
+                    if (lastPressureData._id.toString() !== lastPressureDataId) {
+                        socket.volatile.emit("pressure-data", lastPressureData.value);
+                    }
+                    lastPressureDataId = lastPressureData._id.toString();
                 }
-                lastPressureDataId = lastPressureData._id.toString();
-            }
-            else {
-                if (lastPressureDataId !== "-1") {
-                    const value = {noData: true};
-                    socket.volatile.emit("pressure-data", value);
-                    lastPressureDataId = "-1";
+                else {
+                    if (lastPressureDataId !== "-1") {
+                        const value = {noData: true};
+                        socket.volatile.emit("pressure-data", value);
+                        lastPressureDataId = "-1";
+                    }
                 }
-            }
+            }, 1000);
+            dataIntervalList.push(senderPressureDataInterval);
         }, 3000);
-        dataIntervalList.push(senderPressureDataInterval);
     }
 
     socket.on("disconnect", () => {
