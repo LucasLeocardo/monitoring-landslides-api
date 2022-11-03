98984a689bcb6f43994345010465e8b69fe6cbb3
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-11-02 21:54:59 -0300

    trying to fix redis

--- redis/black-list.js
+++ redis/black-list.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const redis = require('redis');
-const createRedisClient = process.env.REDIS_URL ? createHerokuRedisClient : createLocalRedisClient;
+const createRedisClient = process.env.REDIS_URL || process.env.REDIS_TLS_URL ? createHerokuRedisClient : createLocalRedisClient; 
 
 module.exports = createRedisClient(redis);
 
 
 function createHerokuRedisClient(redis) {
     const redisUrlObj = new URL(process.env.REDIS_URL);
-    const redisClient = redis.createClient({url: process.env.REDIS_URL, no_ready_check: true, prefix: 'blacklist:'});
+    const redisClient = redis.createClient({url: process.env.REDIS_TLS_URL,
+        socket: {
+          tls: true,
+          rejectUnauthorized: false
+        }, no_ready_check: true, prefix: 'blacklist:'});
     //redisClient.auth(redisUrlObj.password);
     return redisClient;
 }
