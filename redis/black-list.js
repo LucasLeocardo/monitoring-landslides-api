0737ea0996778a4aaeb4fe3b2b84ec2029bf36c7
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-11-02 21:46:33 -0300

    trying to fix redis

--- redis/black-list.js
+++ redis/black-list.js
@@ -7,8 +7,8 @@ module.exports = createRedisClient(redis);
 
 function createHerokuRedisClient(redis) {
     const redisUrlObj = new URL(process.env.REDIS_URL);
-    const redisClient = redis.createClient(redisUrlObj.port, redisUrlObj.hostname, { no_ready_check: true }, { prefix: 'blacklist:' });
-    redisClient.auth(redisUrlObj.password);
+    const redisClient = redis.createClient({url: process.env.REDIS_URL, no_ready_check: true, prefix: 'blacklist:'});
+    //redisClient.auth(redisUrlObj.password);
     return redisClient;
 }
 
