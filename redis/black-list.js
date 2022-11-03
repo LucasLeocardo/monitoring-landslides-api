cc7eca0aa6c60dbcf8aba8ea13bdacc4fee7a412
LucasLeocardo <LucasLeocardo@users.noreply.github.com>
2022-11-02 22:13:01 -0300

    trying to fix redis

--- redis/black-list.js
+++ redis/black-list.js
@@ -1,29 +1,35 @@
 require('dotenv').config();
-//const redis = require('redis');
-const redis = require("ioredis");
+const redis = require('redis');
+//const redis = require("ioredis");
 const createRedisClient = process.env.REDIS_URL ? createHerokuRedisClient : createLocalRedisClient; 
 
 module.exports = createRedisClient(redis);
 
 
-// function createHerokuRedisClient(redis) {
-//     const redisUrlObj = new URL(process.env.REDIS_URL);
-//     const redisClient = redis.createClient({url: process.env.REDIS_TLS_URL,
-//         socket: {
-//           tls: true,
-//           rejectUnauthorized: false
-//         }, no_ready_check: true, prefix: 'blacklist:'});
-//     //redisClient.auth(redisUrlObj.password);
-//     return redisClient;
-// }
-
 function createHerokuRedisClient(redis) {
-    return new redis(process.env.REDIS_URL, {
-        tls: {
-            rejectUnauthorized: false
+    //const redisUrlObj = new URL(process.env.REDIS_URL);
+    return redis.createClient({
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: true,
+          rejectUnauthorized: false
         }
     });
+    // return redis.createClient({url: process.env.REDIS_URL,
+    //     socket: {
+    //       tls: true,
+    //       rejectUnauthorized: false
+    //     }, no_ready_check: true, prefix: 'blacklist:'});
+    //redisClient.auth(redisUrlObj.password);
 }
+
+// function createHerokuRedisClient(redis) {
+//     return new redis(process.env.REDIS_URL, {
+//         tls: {
+//             rejectUnauthorized: false
+//         }
+//     });
+// }
 
 function createLocalRedisClient(redis) {
     return redis.createClient({ prefix: 'blacklist:' });
